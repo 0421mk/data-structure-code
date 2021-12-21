8dd313ee4e3497f080c412ab8642982e0524be71
youngbamer <0421mk@naver.com>
2021-12-21 18:26:23 +0900

    양방향 연결리스트 구현 및 예제 제작

--- DBLinkedList/DBLinkedList.h
+++ DBLinkedList/DBLinkedList.h
@@ -8,6 +8,7 @@ typedef int Data;
 
 typedef struct _node
 {
+	// 양방향 연결리스트는 양방향으로 연결되므로 next와 prev 포인터 변수를 사용합니다. 
 	Data data;
 	struct _node * next;
 	struct _node * prev;
@@ -27,7 +28,7 @@ void LInsert(List * plist, Data data);
 
 int LFirst(List * plist, Data * pdata);
 int LNext(List * plist, Data * pdata);
-int LPrevious(List * plist, Data * pdata);
+int LPrevious(List * plist, Data * pdata); // LNext 함수의 반대 방향으로 노드 참조
 int LCount(List * plist);
 
 #endif
--- DBLinkedList/main.c
+++ DBLinkedList/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "DBLinkedList.h"
 
 int main(void) {
+	List list;
+	int data;
+	ListInit(&list);
+	
+	// 1부터 8까지 8개의 데이터를 저장해주세요. 
+	LInsert(&list, 1);
+	LInsert(&list, 2);
+	LInsert(&list, 3);
+	LInsert(&list, 4);
+	LInsert(&list, 5);
+	LInsert(&list, 6);
+	LInsert(&list, 7);
+	LInsert(&list, 8);
+	
+	// 저장된 데이터를 조회해주세요.
+	// 8 7 6 5 5 6 7 8 7 6 5 4 3 2 1 2 3 4 5 6 7 8
+	// 조회된 데이터는 위와 같은 순서를 갖습니다.
+	// LNext와 LPrevious를 이용해서 순서대로 출력해주세요. 
+	int n = 0;
+	if(LFirst(&list, &data)) {
+		printf("%d ", data);
+		
+		while(LNext(&list, &data)) {
+			if(n == 3) {
+				break;
+			}
+			printf("%d ", data);
+			n++;
+		}
+				
+		while(LPrevious(&list, &data)) {
+			printf("%d ", data);
+		}
+		
+		while(LNext(&list, &data)) {
+			printf("%d ", data);
+			n++;
+		}
+		
+		while(LPrevious(&list, &data)) {
+			printf("%d ", data);
+		}
+		
+	}
+	
 	return 0;
 }
