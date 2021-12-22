970edf638ac4c62dbf7bc45d8623cad7394dc4ee
youngbamer <0421mk@naver.com>
2021-12-23 01:27:47 +0900

    배열 기반 스택 구현, 예제 수정

--- ArrayBaseStack/ArrayBaseStack.c
+++ ArrayBaseStack/ArrayBaseStack.c
@@ -5,6 +5,8 @@
 void StackInit(Stack * pstack) // 스택의 초기화
 {
 	pstack->topIndex = -1;
+	// 데이터는 -1로 설정
+	// 데이터를 추가할 때마다 topIndex 1씩 증가 
 }
 
 int SIsEmpty(Stack * pstack) // 스택이 비었는지 확인합니다.
@@ -15,8 +17,24 @@ int SIsEmpty(Stack * pstack) // 스택이 비었는지 확인합니다.
 		return FALSE;
 }
 
+// 인덱스는 0~99까지니까 -1 해주어야 한다.
+// 스택이 꽉 찼는지 확인합니다. 
+int SIsFull(Stack * pstack)
+{ 
+	if (pstack->topIndex == STACK_LEN-1)
+		return TRUE; 
+	else
+		return FALSE;
+}
+
 void SPush(Stack * pstack, Data data) // 스택의 데이터 추가
 {
+	if(SIsFull(pstack))
+	{
+		printf("Stack Overflow!");
+		exit(-1); // 에러 메시지 출력 후 프로세스 종료 
+	}
+	
 	pstack->topIndex += 1;
 	pstack->stackArr[pstack->topIndex] = data;
 }
@@ -28,11 +46,13 @@ Data SPop(Stack * pstack) // 스택의 데이터 삭제
 	if(SIsEmpty(pstack))
 	{
 		printf("Stack Empty!");
-		exit(-1);
+		exit(-1); // 에러 메시지 출력 후 프로세스 종료 
 	}
 	
 	rIdx = pstack->topIndex;
-	pstack->topIndex -= 1; // 삭제 연산 
+	pstack->topIndex -= 1; // 삭제 연산
+	// rIdx의 데이터 값을 0으로 초기화하지 않는 이유: 배열에서 삭제를 위해 0으로 세팅하는건 의미가 없습니다.
+	// 어차피 topIndex 값보다 위에 위치하고 있다면 유효한 데이터로 인정을 못받고 또한 데이터가 삽입될 경우 덮어씌어지면 됩니다. 
 	
 	return pstack->stackArr[rIdx];
 }
--- ArrayBaseStack/ArrayBaseStackMain.c
+++ ArrayBaseStack/ArrayBaseStackMain.c
@@ -7,16 +7,40 @@ int main(void) {
 	StackInit(&stack);
 	
 	// 데이터 넣기
+	// 가장 앞에 있는 값이 topIndex 
 	SPush(&stack, 1); 
 	SPush(&stack, 2);
 	SPush(&stack, 3);
 	SPush(&stack, 4);
 	SPush(&stack, 5);
+	SPush(&stack, 6); 
+	SPush(&stack, 7);
+	SPush(&stack, 8);
+	SPush(&stack, 9);
+	SPush(&stack, 10);
 	
+	// 스택의 모든 데이터를 조회해주세요.
+	// 스택은 최상단 데이터만 조회가능하므로 모든 데이터를 조회하려면 삭제가 수반되어야 합니다. 
+	// SIsEmpty 함수를 이용하면 됩니다. 
 	while(!SIsEmpty(&stack))
 	{
 		printf("%d ", SPop(&stack)); // 가장 마지막에 들어간 값이 가장 먼저 나옵니다. 
 	}
+	
+	printf("\n\n");
+	
+	// 현재 스택에 데이터가 삭제된 상태입니다.
+	// 삭제한 상태에서 데이터를 다시 한번 조회해주세요.
+	int i;
+	
+	for(i=0; i<10; i++) {
+		printf("%d ", stack.stackArr[i]); 
+	}
+	
+	// 사실 데이터가 완전히 삭제되지 않았습니다.
+	// 배열의 삭제 연산은 데이터를 초기화하지 않고 남겨둔 뒤 덮어씌우는 방식을 사용합니다.
+	// (배열은 처음부터 이미 메모리 공간이 배정되어 있으므로) 
+	// 연결리스트는 다릅니다. 
 	
 	return 0;
 }
